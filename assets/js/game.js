f8f73be86380e91b925da03b1f2bce8f76ec3f60
Caborca <pilarbuchen@gmail.com>
2021-02-14 13:38:51 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,6 +68,7 @@ var fight = function(enemyName) {
   }
 };
 
+var startGame = function() {
 for (var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
@@ -90,6 +91,31 @@ if (playerHealth > 0) {
 // if player isn't alive, stop the game
 else {
   window.alert('You have lost your robot in battle! Game Over!');
+} 
+
 }
 
+startGame ();
 
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+}
+
+endGame (); 
+
+// ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
